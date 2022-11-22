8f1aeb5cddb14143a58e1307d02babc5dc4169c8
ChristianVilchezA <christian.servtec@gmail.com>
2022-11-22 18:30:59 -0500

    First commit

---
+++ script7.js
@@ -0,0 +1,29 @@
+let pokemons = [
+    {id: 1, name: "charmander", type: "fire", base_damage: 10, base_hp: 12, speed: 30},
+    {id: 2, name: "squirtle", type: "water", base_damage: 9, base_hp: 14, speed: 26},
+    {id: 3, name: "bulbasaur", type: "leaf", base_damage: 8, base_hp: 16, speed: 26},
+    {id: 4, name: "pikachu", type: "electric", base_damage: 12, base_hp: 8, speed: 32},
+    {id: 5, name: "pidgey", type: "air", base_damage: 10, base_hp: 10, speed: 35},
+    {id: 6, name: "goldeen", type: "water", base_damage: 9, base_hp: 12, speed: 32},
+    {id: 7, name: "bellsprout", type: "leaf", base_damage: 10, base_hp: 12, speed: 30},
+    {id: 8, name: "magnemite", type: "electric", base_damage: 9, base_hp: 14, speed: 30},
+    {id: 9, name: "ponyta", type: "fire", base_damage: 12, base_hp: 18, speed: 36},
+    {id: 10, name: "evee", type: "normal", base_damage: 10, base_hp: 12, speed: 30},
+]
+
+
+//7. Crear una funcion que determine el daño que hara un pokemon elegido de la lista ante una posible pelea, para ello considerar que el daño que hara el pokemon es:
+// daño = base_damage + un valor aleatorio entre el min_damage y el max_damage
+ 
+function setDamage(index){
+    if (index < pokemons.length){
+        addAtributes()
+        let selectPokemon = pokemons[index]
+        let total_damage = selectedPokemon.base_damage + Math.floor(Math.random() * (selectedPokemon.max_damage-1+selectedPokemon.min_damage)+selectPokemon.min_damage)
+    
+        console.log(total_damage)
+    } else{
+        console.log(`Pleae enter a number not greaet than ${pokemon.length - 1}`)
+    }   
+}
+        
